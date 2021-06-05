8bd92eed278210b0080f922dc63fb3d6798b5df1
Krish Goel <krishgoel3@gmail.com>
2021-06-05 12:46:21 +0530

    Trying to fix the Telegram bot

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ require("dotenv").config();
 const axios = require("axios");
 
 function sendMessage(message) {
-	axios(
-		{
-			method: 'post',
-			url: "https://api.telegram.org/bot"+process.env.TOKEN+"/sendMessage?chat_id="+process.env.CHAT_ID+"&text="+message
-		}
-	)
-	.then((response) => {
-		console.log(response);
-	}, (error) => {
-		console.log(error);
-	});
+	axios.post("https://api.telegram.org/bot"+process.env.TOKEN+"/sendMessage?chat_id="+process.env.CHAT_ID+"&text="+message)
+		.then(function(response) {
+			console.log(response);
+		})
+		.catch(function(error) {
+			console.log(error);
+		});
 }
 
 sendMessage("Lrish")
