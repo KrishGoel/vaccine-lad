d89d47314a6393be25188e16f79cd68cec3f12eb
Krish Goel <krishgoel3@gmail.com>
2021-06-05 14:17:52 +0530

    Testing for env

--- index.py
+++ index.py
@@ -8,13 +8,15 @@ chatid = os.getenv("CHAT_ID")
 
 import requests
 
+print("Token:", token)
+
 # GET Req examples
 # response = requests.get("https://potion-api.vercel.app/table?id=ed5a59a2d5924e6f81044a1ac9247f97")
 # print(response.json())
 
-def sendMessage(message):
-    url = 'https://api.telegram.org/bot'+str(token)+'/sendMessage?chat_id='+str(chatid)+'&text='+str(message)
-    x = requests.post(url)
-    print(x.text)
+# def sendMessage(message):
+#     url = 'https://api.telegram.org/bot'+str(token)+'/sendMessage?chat_id='+str(chatid)+'&text='+str(message)
+#     x = requests.post(url)
+#     print(x.text)
 
-sendMessage("Krsh")
+# sendMessage("Krsh")
