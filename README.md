df2927631164b8b7d5cb0ed04ef4076ca87646e0
Krish Goel <krishgoel3@gmail.com>
2021-06-05 13:20:30 +0530

    Transitioned to Python

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Vaccine Lad
 
 ## Notes
-- Enabled low-security apps on sender's email because of GMail Security
-- Now following https://dev.to/chandrapantachhetri/sending-emails-securely-using-node-js-nodemailer-smtp-gmail-and-oauth2-g3a to fix the issues with nodemailer on GH Actions
---
+++ index.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('./')
+load_dotenv(os.path.join(project_folder, '.env'))
+token = os.getenv("TOKEN")
+chatid = os.getenv("CHAT_ID")
+
+import requests
+
+# GET Req examples
+# response = requests.get("https://potion-api.vercel.app/table?id=ed5a59a2d5924e6f81044a1ac9247f97")
+# print(response.json())
+
+def sendMessage(message):
+    url = 'https://api.telegram.org/bot'+str(token)+'/sendMessage?chat_id='+str(chatid)+'&text='+str(message)
+    x = requests.post(url)
+    print(x.text)
+
+sendMessage("Krsh")
