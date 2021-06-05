52bf4a4dc7b5a439974f2fb7c3ac68fe171d15cd
Krish Goel <krishgoel3@gmail.com>
2021-06-05 14:39:08 +0530

    Experimenting with Github secrets

--- index.py
+++ index.py
@@ -4,22 +4,22 @@ import os
 # For localdev
 # project_folder = os.path.expanduser('./')
 # load_dotenv(os.path.join(project_folder, '.env'))
-# token = os.getenv("TOKEN")
-# chatid = os.getenv("CHAT_ID")
+# TOKEN = os.getenv("TOKEN")
+# CHAT_ID = os.getenv("CHAT_ID")
 
-token = os.environ.get("TOKEN")
-chatid = os.environ.get("CHAT_ID")
+TOKEN = os.environ.get("TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 import requests
 
-print("Token:", token)
+print("Token:", TOKEN)
 
 # GET Req examples
 # response = requests.get("https://potion-api.vercel.app/table?id=ed5a59a2d5924e6f81044a1ac9247f97")
 # print(response.json())
 
 # def sendMessage(message):
-#     url = 'https://api.telegram.org/bot'+str(token)+'/sendMessage?chat_id='+str(chatid)+'&text='+str(message)
+#     url = 'https://api.telegram.org/bot'+str(TOKEN)+'/sendMessage?chat_id='+str(CHAT_ID)+'&text='+str(message)
 #     x = requests.post(url)
 #     print(x.text)
 
